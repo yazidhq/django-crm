661be646244213f630e7cd3d4a2fea922012b8ef
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-17 16:38:23 +0700

    Auth Login

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+# website app
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+]
