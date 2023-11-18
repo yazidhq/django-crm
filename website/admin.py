65d47fb014a276528b84b07737abcca47a4a8fa6
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-18 21:43:22 +0700

    Admin

--- website/admin.py
+++ website/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Record
 
 # Register your models here.
-admin.site.register(Record)
+
+# show only firstname and lastname (check models.py def __str__)
+# admin.site.register(Record)
+
+
+# showing depend list display
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'email', 'address', 'created_at']
